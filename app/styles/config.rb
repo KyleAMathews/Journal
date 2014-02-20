077f17eba3ded20697d059cc09cb4e79a0565c78
Kyle Mathews <mathews.kyle@gmail.com>
2014-02-20 02:58:22 +0000

    Ruby 1.9+ doesn't compile correctly w/o encoding set

--- app/styles/config.rb
+++ app/styles/config.rb
@@ -9,6 +9,8 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+Encoding.default_external = "UTF-8"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
