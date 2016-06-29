117bff745e12b31b5c20e9670c0ba703f0ebe639
Kyle Mathews <mathews.kyle@gmail.com>
2016-06-29 00:11:31 -0700

    Upgrade to latest Typography.js

--- src/typography.js
+++ src/typography.js
@@ -1,12 +1,21 @@
 import Typography from 'typography'
+import gray from 'gray-percentage'
 
 export const typography = new Typography({
-  headerFontFamily: '"Clear Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  headerFontFamily: ['Clear Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
   headerGray: 15,
   bodyGray: 30,
-  bodyFontFamily: '"Linux Libertine", Georgia, sans-serif',
+  bodyFontFamily: ['Linux Libertine', 'Georgia', 'sans-serif'],
   baseFontSize: '20px',
-  baseLineHeight: '26px',
+  baseLineHeight: '27px',
+  overrideStyles: ({ adjustFontSizeTo, rhythm }) => ({
+    blockquote: {
+      ...adjustFontSizeTo('22px'),
+      borderLeft: `${rhythm(1/4)} solid ${gray(87)}`,
+      marginLeft: 0,
+      paddingLeft: rhythm(3/4),
+    },
+  }),
 })
 
 typography.injectStyles()
